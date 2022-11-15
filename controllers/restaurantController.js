f4944c1fd02c1bd0f87fb56a7b144e6d1d497183
zqjo29 <zqjo@fresnou.org>
2022-11-14 18:26:23 -0800

    finished!(?) :D

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,8 +1,28 @@
 const {Restaurant} = require('../models');
 const categories = ['Italian', 'Fast Food', 'Chinese', 'Japanese', 'Restaurant'];
-module.exports.viewAll = async function(req, res, next) {
-    const restaurants = await Restaurant.findAll();
-    res.render('index', {restaurants});
+
+module.exports.viewAll = async function(req, res) {
+    let searchCategories = ['All'];
+    for(let i = 0; i < categories.length; i++) {
+        searchCategories.push(categories[i]);
+    }
+    let restaurants;
+    let searchCategory = req.query.category || 'All';
+    let searchRandom = req.query.random || false;
+    if (searchCategory === 'All') {
+        restaurants = await Restaurant.findAll();
+    } else {
+        restaurants = await Restaurant.findAll({
+            where: {
+                category: searchCategory
+            }
+        });
+    }
+    if (restaurants.length > 0 && searchRandom) {
+        let randomIndex = getRandomInt(restaurants.length);
+        restaurants = [restaurants[randomIndex]];
+    }
+    res.render('index', {restaurants, categories:searchCategories, searchCategory, searchRandom});
 }
 
 module.exports.renderEditForm = async function(req, res, next) {
@@ -63,6 +83,10 @@ module.exports.addRestaurant = async function(req, res) {
             description: req.body.description
         });
     res.redirect('/');
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
 
 // const restaurants = [{
