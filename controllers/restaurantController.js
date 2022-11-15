07c4b1adb334b717ec3e2683e51bc3193d295556
zqjo <zqjo@fresnou.org>
2022-11-15 11:22:00 -0800

    starting pt. 3 :D (there's some console.logs on restaurantController, delete those when done)

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -2,27 +2,37 @@ const {Restaurant} = require('../models');
 const categories = ['Italian', 'Fast Food', 'Chinese', 'Japanese', 'Restaurant'];
 
 module.exports.viewAll = async function(req, res) {
+    // let searchCategories = ['All'];
+    // for(let i = 0; i < categories.length; i++) {
+    //     searchCategories.push(categories[i]);
+    // }
+    // let restaurants;
+    // let searchCategory = req.query.category || 'All';
+    // let searchRandom = req.query.random || false;
+    // console.log(searchCategory);
+    // if (searchCategory === 'All') {
+    //     restaurants = await Restaurant.findAll();
+    // } else {
+    //     restaurants = await Restaurant.findAll({
+    //         where: {
+    //             category: searchCategory
+    //         }
+    //     });
+    // }
+    // if (restaurants.length > 0 && searchRandom) {
+    //     let randomIndex = getRandomInt(restaurants.length);
+    //     restaurants = [restaurants[randomIndex]];
+    // }
+    // res.render('index', {restaurants, categories:searchCategories, searchCategory, searchRandom});
+    const restaurants = await Restaurant.findAll();
+    let searchCategory = 'All';
     let searchCategories = ['All'];
-    for(let i = 0; i < categories.length; i++) {
+    for (let i = 0; i < categories.length; i++) {
         searchCategories.push(categories[i]);
     }
-    let restaurants;
-    let searchCategory = req.query.category || 'All';
-    let searchRandom = req.query.random || false;
-    if (searchCategory === 'All') {
-        restaurants = await Restaurant.findAll();
-    } else {
-        restaurants = await Restaurant.findAll({
-            where: {
-                category: searchCategory
-            }
-        });
-    }
-    if (restaurants.length > 0 && searchRandom) {
-        let randomIndex = getRandomInt(restaurants.length);
-        restaurants = [restaurants[randomIndex]];
-    }
-    res.render('index', {restaurants, categories:searchCategories, searchCategory, searchRandom});
+   console.log(searchCategories);
+   console.log(searchCategory);
+   res.render('index', {restaurants, categories:searchCategories, searchCategory});
 }
 
 module.exports.renderEditForm = async function(req, res, next) {
